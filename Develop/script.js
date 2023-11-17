04098c882527708385ca13d45c63d8413275ad50
Alex Place <alexander.n.place@gmail.com>
2023-11-16 21:09:46 -0500

    Updated JavaScript code

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,43 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+let specialCharacter = ' !#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+let passNumber = '0123456789';
+
+// randomPassword = all above values
+// Generate password functionality
+
+function userChoice(){
+  var choice = confirm("Click OK to include an upper case letter.")
+  
+  if (choice === true){
+    writePassword += upperCase[randomIndex];
+  }
+
+// if user picks all false options, return statement that they have to choose something
+}
+
+function generatePassword() {  
+  // if else statements within here
+  var randomIndex = Math.floor(Math.random() * randomPassword.length)
+  userChoice();
+  // console.log(choice)
+// return "password1"
+
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+// console.log(passwordText)
   passwordText.value = password;
 
+// console.log(passwordText.value)
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
